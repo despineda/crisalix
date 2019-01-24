96d474b7c8aead9949e5320afc352a6fd324d570
Maria Lourdes <Maria Lourdes>
2019-01-24 16:11:41 +0800

    new

--- test-project/resources/views/welcome.blade.php
+++ test-project/resources/views/welcome.blade.php
@@ -14,9 +14,8 @@
         <!-- bootstrap -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
-
         <!-- custom stylesheet -->
-        <link rel="stylesheet" type="text/css" href="/css/career.css">
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
 
     </head>
     <body>
@@ -50,9 +49,9 @@
             </nav>
     
     <!-- banner -->
-    <div class="jumbotron jumbotron-fluid text-center">
+    <div class="banner-career jumbotron jumbotron-fluid text-center">
         <div class="container">
-            <h1 class="display-4">Join Our <span class="team">Team</span></h1>
+            <h1 class="display-4 join">Join Our <span class="team">Team</span></h1>
             <p class="lead">"Our Goal is to focus on you while you focus on growth"</p>
         </div>
     </div>
@@ -110,11 +109,42 @@
 <div class="jumbotron-subscribe jumbotron-fluid text-center">
   <div class="container subscribe text-center">
     <h1 class="subscribe-text">Subscribe to Our <span class="news">Newsletter</span></h1>
-    <!-- <input type="text" class="form-control" placeholder="Enter Your Email Address" aria-label="Recipient's username" aria-describedby="basic-addon2" ><button type="button" class="btn btn-primary submit-btn">Primary</button> -->
+    <input type="text" class="form-control form-email" placeholder="Enter your email address" aria-label="Recipient's username" aria-describedby="basic-addon2" ><button type="button" class="btn btn-primary submit-btn">Submit</button>
   </div>
 </div>   
 
-
+<!-- get in touch section -->
+<div class="container-fluid get-intouch text-center">
+    <h1 class="contact-text"><span class="get-text">Get In Touch </span>With Us</h1>
+    <p class="reach-us">Your concern is very important for us. You can reach us through<br><span class="support">support@republisys.com </span>or fill in the online form</p>
+    <div class="row form-row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6 contact-form">
+            <form id="contact" method="post" class="form" role="form">
+            <div class="row">
+                <div class="col-xs-6 col-md-6 form-group">
+                    <input class="form-control" id="name" name="name" placeholder="Enter full name" type="text" required autofocus />
+                </div>
+                <div class="col-xs-6 col-md-6 form-group">
+                    <input class="form-control" id="email" name="email" placeholder="Enter email address" type="email" required />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6 col-md-6 form-group">
+                    <input class="form-control" id="name" name="name" placeholder="Contact number" type="text" required autofocus />
+                </div>
+                <div class="col-xs-6 col-md-6 form-group">
+                    <input class="form-control" id="email" name="email" placeholder="Subject" type="email" required />
+                </div>
+            </div>
+            <textarea class="form-control" id="message" name="message" placeholder="Enter your message" rows="5"></textarea>
+            <br />
+            <div class="row">
+                <div class="col-xs-12 col-md-12 form-group">
+                    <button class="btn btn-primary pull-right button-submit" type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
         <!--  Javascript, Jquery and Popper-->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
